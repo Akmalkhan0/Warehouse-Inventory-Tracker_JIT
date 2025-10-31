3134aa3292b9944936bbaa36e853792f5d040db0
Akmal khan <Akmalkhann0@gmail.com>
2025-10-31 14:23:41 +0530

    full task compelete

--- src/main/java/com/akmal/observer/AlertService.java
+++ src/main/java/com/akmal/observer/AlertService.java
@@ -3,14 +3,9 @@ package com.akmal.observer;
 import com.akmal.model.InventoryItem;
 
 public class AlertService implements StockObserver{
-<<<<<<< HEAD
     public void onLowStock(InventoryItem item){
-        System.out.println("Low Stock Of The Product : "+item.getProduct().getName());
-=======
-    public void onLowStock(Product product){
-        if(product.getReorder() > product.getQuantity()){
-            System.out.println("Low Stock Of The Product : "+product.getName());
+        if(item.getReorder() > item.getQuantity()){
+            System.out.println("Low Stock Of The Product : "+item.getProduct().getName());
         }
->>>>>>> 3eef18ac1ea5c3c2b7562c08eff23c2a148d170e
     }
 }
